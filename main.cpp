26295cc54af520752c6d3a5704248c81eb6db3f9
Vinay Reddy Ratnam <vnratnan@gmail.com>
2025-06-04 23:42:30 -0400

    Created new way to model states.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,30 @@
 //
 // Created by Vinay Reddy Ratnam on 6/3/25.
 //
-int main();
+#include "player.h"
+#include "board.h"
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int choice;
+    cout << "Please choose what you would like to do. Enter an integer value:" << endl;
+    cout << "1. Train bots using Monte Carlo Simulations" << endl;
+    cout << "2. Train bots using Q-Learning" << endl;
+    cout << "3. Play against bot as Xs" << endl;
+    cout << "4. Play against bot as Os" << endl;
+
+    cin >> choice;
+
+    switch (choice) {
+        case 1:
+
+        case 2:
+
+        case 3:
+
+        case 4:
+
+    }
+};
--- player.h
+++ player.h
@@ -11,75 +11,43 @@
 #include <algorithm>
 using namespace std;
 
-class Players() {
+class Players {
 public:
-    map<string, map<char, float>> action_values;
-    vector<char> all_actions {a, b, c, d, e, f, g, h, i};
+    map<string, map<int, float>> action_values;
+
     int move = 0;
+    vector<int> rewards {-1,0,-1};
 
-    Player() {
-        vector<string> states;
-        states.push_back("");
+    Player() { //default constructor should initialize states for training
+        // create states
+        pair<string, pair<int, int>> initial_state {"", {0,0}};
+        createStates(initial_state);
+    }
 
-        states.insert(states.end(), makeStatesAndActions("")); // should give all the permutations of states
+    trainWithMC
 
-        // let's extract all the combos; order is not important (to an extent)
-        for (auto state : states) {
-            vector<char> player_1_moves;
-            vector<char> player_2_moves;
-            for (int i = 0; i < state.size(); i+=2) {
-
-                player_1_moves.push_back(state[i]);
-
-                if (i+1 < state.size()) {
-                    player_2_moves.push_back(state[i+1]);
+private:
+    void createStates(pair<string, pair<int,int>> curr_state) {
+        if (curr_state.first.size() == 9) {
+            //this is a finalized state
+            //calculate the possible actions for said state
+            map<int, float> actions;
+            for (int i = 0; i < 9; i++) {
+                if (curr_state.first[i] == 'a') {
+                    actions[i] = 0.0;
                 }
             }
-
-            sort(player_1_moves.begin(), player_1_moves.end());
-            sort(player_2_moves.begin(), player_2_moves.end());
-
-            string final_form;
-            int index = 0;
-            while (index < player_2_moves.size()) {
-                //size of player_1_moves will always be greater than or equal to size of player_2_moves
-                final_form.push_back(player_1_moves[index]);
-                final_form.push_back(player_2_moves[index++]);
-            }
-            if (index + 1 == player_1_moves.size() - 1) {
-                final_form.push_back(player_1_moves[index+1]);
-            }
-
-            //final_form now has the desired convention to describe the state
-            vector<char> extra_states
+            action_values[curr_state.first] = actions;
         }
 
+        else {
+            if (curr_state->second.first < 5) {
+                createStates({curr_state.first + 'b', {curr_state.second.first+1, curr_state.second.second}});
+            }
+            if (curr_state->second->second < 4) {
+                createStates({curr_state.first + 'c', {curr_state.second.first, curr_state.second.second+1}});
+            }
+            createStates({curr_state.first + 'a', curr_state.second});
+        }
     }
-
-    vector<string> makeStatesAndActions(string state) {
-        if (state.size() == 9) {
-            return {};
-        }
-
-        set<char> actions_taken;
-        for (int i = 0; i < state.size(); i++) {
-            actions_taken.insert(state[i]);
-        }
-
-        vector<string> new_states;
-        for (char i = 97; i < 97+9; i++) {
-            if (actions_taken.find(i) == actions_taken.end()) {
-                //character was not found in previous actions; it can be taken
-                new_states.push_back(state + i);
-            }
-        }
-
-        for (auto state : new_states) {
-            new_states.insert(new_states.end(), makeStatesAndActions(state));
-        }
-
-        return new_states;
-    }
-
-
-}
+};
